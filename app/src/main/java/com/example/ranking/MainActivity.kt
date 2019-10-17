bcb5b4dd0c55124de20bf9df1f103c46930d5b67
kaisei0326 <kaisei0326@gmail.com>
2019-10-17 10:39:45 +0900

    [fix]ランキングを実際のものに対応させた

--- app/src/main/java/com/example/ranking/MainActivity.kt
+++ app/src/main/java/com/example/ranking/MainActivity.kt
@@ -7,7 +7,9 @@ import com.github.kittinunf.fuel.*
 import com.github.kittinunf.fuel.core.*
 import com.github.kittinunf.fuel.coroutines.*
 import com.github.kittinunf.result.Result
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
@@ -15,10 +17,19 @@ import kotlinx.coroutines.*
 class MainActivity : AppCompatActivity() {
 
     data class TestModel(
-        var qty: Int
+        val qty: Int
     )
 
-    private val tag = MainActivity::class.java.simpleName
+    data class Top(
+        val profile: Profile,
+        val rank: Int
+    )
+
+    data class Profile(
+        val name: String,
+//        val twitterId: String,
+        val score: Int
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,26 +40,26 @@ class MainActivity : AppCompatActivity() {
             baseHeaders = mapOf("Device" to "Android")
             baseParams = listOf("key" to "value")
         }
-        getRanking.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) {
-                httpGet()
-            }
-        }
 
+        GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) {
+            showRanking()
+        }
     }
 
-    private suspend fun httpGet() {
-        val (request, response, result) = "http://192.168.43.230:8080/ranking/qty".httpGet().awaitStringResponseResult()
-        Log.d(tag, response.toString())
-        Log.d(tag, request.toString())
+
+    private suspend fun showRanking() {
+        val (_, response, result) = Fuel.get("/ranking/top").awaitStringResponseResult()
+        Log.d(MainActivity::class.java.simpleName, response.toString())
         update(result)
     }
 
     private fun <T : Any> update(result: Result<T, FuelError>) {
         result.fold(success = {
             val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-            val TestModel = moshi.adapter(TestModel::class.java).fromJson(it.toString())
-            mainResultText.append("qry -> ${TestModel?.qty}")
+            val listMyData = Types.newParameterizedType(List::class.java, Top::class.java)
+            val jsonAdapter: JsonAdapter<List<Top>> = moshi.adapter(listMyData)
+            val Top = jsonAdapter.fromJson(it.toString())
+           Top!!.forEach{ element -> mainResultText.append("${element.rank}位 name: ${element.profile.name} score: ${element.profile.score}\n") }
         }, failure = {
             mainResultText.append(String(it.errorData))
         })
